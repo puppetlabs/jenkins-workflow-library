ba40ab61569339e0f9123cc96eaebfb4834ab681
Carl Caum <carl@carlcaum.com>
2016-05-13 13:55:51 -0700

    Complete working POC

--- puppet_enterprise.groovy
+++ puppet_enterprise.groovy
@@ -1,10 +1,17 @@
 import groovy.json.*
 
+def json_parser(string) {
+  def slurper = new JsonSlurper()
+  return slurper.parseText(string)
+}
+
 def pe_request( Map parameters = [:], String endpoint ) {
   assert parameters.port instanceof Integer
-  assert parameters.endpoint instanceof String
 
-  def curl_cmd = "curl --cacert /var/lib/jenkins/ca.pem https://${puppet_master}:${parameters.port}/${endpoint}"
+  sh "/opt/puppet/bin/puppet config print server --config /etc/puppetlabs/puppet/puppet.conf"
+  def puppet_master = readFile('return_body').trim()
+
+  def curl_cmd = "curl --cacert /var/lib/jenkins/ca.pem https://${puppet_master}:${parameters.port}${endpoint}"
 
   if (parameters.method) {
     assert parameters.method instanceof String
@@ -12,41 +19,34 @@ def pe_request( Map parameters = [:], String endpoint ) {
   } else {
     parameters.method = 'GET'
   }
-  curl_cmd = curl_cmd + " -X ${parameters.method} -H 'Content-Type: application/json "
-
+  curl_cmd = curl_cmd + " -X ${parameters.method} -H 'Content-Type: application/json' "
   if (parameters.body) {
     assert parameters.body instanceof Map
     parameters.body = new JsonBuilder(parameters.body).toString()
-    curl_cmd = curl_cmd + " -d ${parameters.body} "
+    curl_cmd = curl_cmd + " -d '${parameters.body}' "
   }
 
   def token = readFile('/var/lib/jenkins/.puppetlabs/token').trim()
   curl_cmd = curl_cmd + " -H 'X-Authentication: ${token}' "
 
-  sh "cat /etc/puppetlabs/puppet/puppet.conf  | grep server | cut -d' ' -f3 > return_body"
-  def puppet_master = readFile('return_body').trim()
+  curl_cmd = curl_cmd + " > return_body"
 
-  sh "${curl_cmd} > return_body"
+  sh curl_cmd
   def return_body = readFile('return_body').trim()
 
   def result = json_parser(return_body)
 
-  if (result.error) {
+  if (result instanceof Map && result.error) {
     throw new Exception("Error creating Puppet Enterprise job: ${result.error}")
   }
 
   return result
 }
 
-def json_parser(string) {
-  def slurper = new JsonSlurper()
-  return slurper.parseText(string)
-}
-
 def run( Map parameters = [:], String environment ) {
   def api = '/orchestrator/v1'
 
-  def request_body = ['environment': environment, 'noop': false]
+  def request_body = ["environment": environment, "noop": false]
 
   if (parameters.target) {
     assert parameters.target instanceof String
@@ -66,16 +66,18 @@ def run( Map parameters = [:], String environment ) {
   return pe_request("${api}/command/deploy", port: 8143, method: 'POST', body: request_body)
 }
 
-def deployCode( environments = 'all' ) {
+def deployCode(environments = 'all' ) {
   def api = '/code-manager/v1'
-  def request_body = [:]
+  def request_body = ["wait": true]
 
-  if (environment == 'all') {
-    request_body['deploy-all'] = true
+  if (environments == 'all') {
+    request_body["deploy-all"] = true
+  } else if (environments instanceof List || environments instanceof String) {
+    request_body["environments"] = environments
   } else {
-    request_body['environments'] = environments
+    throw new Exception("Provided environments in unkonwn format. Should be List or String")
   }
-    
+
   pe_request("${api}/deploys", port: 8170, method: 'POST', body: request_body)
 }
 
